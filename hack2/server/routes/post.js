3f6f3e7d7b5a9483467a6461d5a73424112474b0
b08505024 <ips73519680@gmail.com>
2021-12-07 11:45:23 +0800

    11:45

---
+++ hack2/server/routes/post.js
@@ -0,0 +1,89 @@
+import express from 'express'
+import post from '../models/post'
+import moment from 'moment'
+
+const router = express.Router()
+
+// TODO 2-(1): create the 1st API (/api/allPosts)
+
+
+router.get('/allPosts', async function (req, res) {
+    const posts = await post.find()
+    if (posts.length === 0) {
+        console.log("not!")
+        res.json({
+            "message": "error",
+            "data": null
+        })
+        res.status(406).send({ message: 'error' })
+    }
+    else {
+        console.log("good!")
+
+        {
+            for (let i = 0; i < posts.length; i++) {
+                posts.sort(function (x, y) {
+                    return y.timestamp - x.timestamp;
+                })
+            }
+        }
+        res.json({
+            "message": "success",
+            "data": posts
+        })
+        res.status(200).send({ message: 'success' })
+    }
+});
+// TODO 3-(1): create the 2nd API (/api/postDetail)
+
+router.get('/postDetail', async function (req, res) {
+    const { queryString } = req.query
+    const data = await post.find(queryString)
+    if (posts.length === 0) {
+        console.log("not!")
+
+
+        res.status(406).send({
+            "message": "error",
+            "post": null
+        })
+    }
+    else {
+        res.json()
+        res.status(200).send({
+            "message": "success",
+            "post": data
+        })
+    }
+})
+// TODO 4-(1): create the 3rd API (/api/newPost)
+router.post('/newPost', async function (req, res) {
+    const { postId,
+        title,
+        content,
+        timestamp } = req.body
+    if (posts.length === 0) {
+        console.log("not!")
+        res.status(406).send({
+            "message": "error",
+            "post": null
+        })
+    }
+    else {
+        const newPost = new post({
+            postId,
+            title,
+            content,
+            timestamp
+        })
+        console.log("create new post");
+        await newPost.save();
+
+        res.status(200).send({
+            "message": "success",
+        })
+    }
+})
+// TODO 5-(1): create the 4th API (/api/post)
+
+export default router
